ca38886929a2dd4cb958d9baf3306ef7ede3eedc
David S <dshi2008mayo@gmail.com>
2026-03-01 11:10:47 -0500

    duck ani

--- quack-quest/character_body_2d.gd
+++ quack-quest/character_body_2d.gd
@@ -1,12 +1,27 @@
-
-extends CharacterBody2D
-
 @export var speed = 400
-
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+@onready var animations = $AnimatedSprite2D 
 
 func _physics_process(delta):
-	get_input()
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	
+	update_animation(input_direction)
 	move_and_slide()
+
+func update_animation(dir):
+	if dir == Vector2.ZERO:
+		animations.play("idle")
+	else:
+
+		if abs(dir.x) > abs(dir.y):
+
+			if dir.x > 0:
+				animations.play("right_walk")
+			else:
+				animations.play("left_walk")
+		else:
+			# Vertical movement
+			if dir.y > 0:
+				animations.play("walk_down") # Front
+			else:
+				animations.play("walk_up")   # Back
