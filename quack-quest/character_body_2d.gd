db179dff978cf11f02f510d97015c38fdeb3172d
Is0toi <143647830+Is0toi@users.noreply.github.com>
2026-03-01 11:21:27 -0500

    Everything looks better in folders

--- quack-quest/character_body_2d.gd
+++ quack-quest/character_body_2d.gd
@@ -1,27 +1,26 @@
+extends CharacterBody2D  # This fixes the 'velocity' and 'move_and_slide' errors
+
 @export var speed = 400
+
 @onready var animations = $AnimatedSprite2D 
 
 func _physics_process(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+
 	velocity = input_direction * speed
-	
-	update_animation(input_direction)
 	move_and_slide()
+
+	update_animation(input_direction)
 
 func update_animation(dir):
 	if dir == Vector2.ZERO:
 		animations.play("idle")
 	else:
-
 		if abs(dir.x) > abs(dir.y):
-
-			if dir.x > 0:
-				animations.play("right_walk")
-			else:
-				animations.play("left_walk")
+			animations.play("walk_right")
+			animations.flip_h = dir.x < 0
 		else:
-			# Vertical movement
 			if dir.y > 0:
-				animations.play("walk_down") # Front
+				animations.play("walk_down")
 			else:
-				animations.play("walk_up")   # Back
+				animations.play("walk_up")
